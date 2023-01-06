987bbbfa295c460aee71748ea07a9501716dc8eb
Subhadiptech <subhadipdhn@gmail.com>
2023-01-07 04:14:23 +0530

    initialized explore fragment with view binding

--- WW/app/src/main/java/com/ersubhadip/presenter/ExploreFragment.kt
+++ WW/app/src/main/java/com/ersubhadip/presenter/ExploreFragment.kt
@@ -6,8 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ersubhadip.ww.R
+import com.ersubhadip.ww.databinding.FragmentExploreBinding
 
 class ExploreFragment : Fragment() {
+    lateinit var binding: FragmentExploreBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +20,8 @@ class ExploreFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore, container, false)
+        binding = FragmentExploreBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
