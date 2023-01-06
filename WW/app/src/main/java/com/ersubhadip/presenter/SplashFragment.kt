47872f1b3cf435d42b2963e7854d42e9d82aab41
Subhadiptech <subhadipdhn@gmail.com>
2023-01-06 23:09:29 +0530

    splash fragment done

--- WW/app/src/main/java/com/ersubhadip/presenter/SplashFragment.kt
+++ WW/app/src/main/java/com/ersubhadip/presenter/SplashFragment.kt
@@ -5,10 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout.DispatchChangeEvent
+import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import com.ersubhadip.ww.R
+import com.ersubhadip.ww.databinding.FragmentSplashBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashFragment : Fragment() {
-
+    private lateinit var binding: FragmentSplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,14 +24,20 @@ class SplashFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        binding = FragmentSplashBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(2400)
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+        }
+
+        Glide.with(requireContext()).load(R.drawable.ic_splash).into(binding.img)
     }
 }
