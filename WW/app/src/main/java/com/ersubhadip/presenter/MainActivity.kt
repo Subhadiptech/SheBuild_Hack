8fbaf56ad00fad87f49bfbfa8ad92bc3c4a1db11
Subhadiptech <subhadipdhn@gmail.com>
2023-01-06 23:00:26 +0530

    navigation setup done

--- WW/app/src/main/java/com/ersubhadip/presenter/MainActivity.kt
+++ WW/app/src/main/java/com/ersubhadip/presenter/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ersubhadip.ww
+package com.ersubhadip.presenter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ersubhadip.ww.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
