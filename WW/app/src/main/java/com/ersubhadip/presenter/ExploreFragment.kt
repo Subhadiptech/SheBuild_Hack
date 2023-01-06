ee9f2d28f89ba375d4399d19939656571cedf799
Subhadiptech <subhadipdhn@gmail.com>
2023-01-07 04:27:37 +0530

    explore tab set done

--- WW/app/src/main/java/com/ersubhadip/presenter/ExploreFragment.kt
+++ WW/app/src/main/java/com/ersubhadip/presenter/ExploreFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.ersubhadip.ww.R
 import com.ersubhadip.ww.databinding.FragmentExploreBinding
 
@@ -26,5 +27,37 @@ class ExploreFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //custom click on tabs
+        binding.tab1.setOnClickListener {
+            binding.tab1.setBackgroundDrawable(
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.tab_filled
+                )
+            )
+            binding.tab2.setBackgroundDrawable(
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.tab_hollow
+                )
+            )
+            //todo:show data
+
+        }
+
+        binding.tab2.setOnClickListener {
+            binding.tab1.setBackgroundDrawable(
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.tab_hollow
+                )
+            )
+            binding.tab2.setBackgroundDrawable(
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.tab_filled
+                )
+            )
+        }
     }
 }
