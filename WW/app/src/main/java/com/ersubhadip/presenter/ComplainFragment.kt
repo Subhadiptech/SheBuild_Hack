c2533a67e529bcbc2cada2f39d7af0a248a2e15b
Subhadiptech <subhadipdhn@gmail.com>
2023-01-08 05:26:33 +0530

    complain section done

--- WW/app/src/main/java/com/ersubhadip/presenter/ComplainFragment.kt
+++ WW/app/src/main/java/com/ersubhadip/presenter/ComplainFragment.kt
@@ -1,30 +1,48 @@
 package com.ersubhadip.presenter
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ersubhadip.ww.R
+import com.ersubhadip.ww.databinding.FragmentComplainBinding
+import com.ersubhadip.ww.databinding.FragmentExploreBinding
 
 class ComplainFragment : Fragment() {
+    lateinit var binding: FragmentComplainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        }
+    }
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_complain, container, false)
+        binding = FragmentComplainBinding.inflate(layoutInflater)
+        return binding.root
 
-}
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        //clicks
+        binding.complainBtn1.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse("https://ncwapps.nic.in/onlinecomplaintsv2/")
+            startActivity(i)
+        }
+
+        binding.complainBtn2.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse("http://www.shebox.nic.in/")
+            startActivity(i)
+        }
     }
 }
