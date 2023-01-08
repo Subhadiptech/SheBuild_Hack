8ca4c65688435901a8a5f02ff78cda343776a8fb
Subhadiptech <subhadipdhn@gmail.com>
2023-01-08 06:16:18 +0530

    sos added every screen

--- WW/app/src/main/java/com/ersubhadip/presenter/ComplainFragment.kt
+++ WW/app/src/main/java/com/ersubhadip/presenter/ComplainFragment.kt
@@ -1,5 +1,6 @@
 package com.ersubhadip.presenter
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
@@ -7,11 +8,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.ersubhadip.helpers.Storage
 import com.ersubhadip.ww.R
 import com.ersubhadip.ww.databinding.FragmentComplainBinding
 import com.ersubhadip.ww.databinding.FragmentExploreBinding
+import java.net.URLEncoder
 
 class ComplainFragment : Fragment() {
+    private lateinit var storage: Storage
     lateinit var binding: FragmentComplainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,11 +30,25 @@ class ComplainFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentComplainBinding.inflate(layoutInflater)
+        storage = Storage(requireContext())
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.sosBtn.setOnClickListener {
+            if (storage.lat.isNotEmpty() && storage.long.isNotEmpty()) {
+                sos()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Location Permission not provided",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
 
         //clicks
         binding.complainBtn1.setOnClickListener {
@@ -42,6 +61,35 @@ class ComplainFragment : Fragment() {
             val i = Intent(Intent.ACTION_VIEW)
             i.data = Uri.parse("http://www.shebox.nic.in/")
             startActivity(i)
+        }
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun sos() {
+        Toast.makeText(
+            requireContext(),
+            "Message sent to ${storage.number}",
+            Toast.LENGTH_SHORT
+        ).show()
+
+        try {
+            val packageManager = requireContext().packageManager
+            val i = Intent(Intent.ACTION_VIEW)
+            val url =
+                "https://wa.me/91${storage.number}" + "?text=" + URLEncoder.encode(
+                    "https://www.google.com/maps?t=m&q=loc:${storage.lat},${storage.long}",
+                    "utf-8"
+                )
+            i.setPackage("com.whatsapp")
+            i.data = Uri.parse(url)
+            requireContext().startActivity(i)
+
+        } catch (e: Exception) {
+            Toast.makeText(
+                requireContext(),
+                "Whatsapp not found!",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
--- WW/app/src/main/java/com/ersubhadip/presenter/ExploreFragment.kt
+++ WW/app/src/main/java/com/ersubhadip/presenter/ExploreFragment.kt
@@ -1,18 +1,24 @@
 package com.ersubhadip.presenter
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ersubhadip.domains.dto.adapterModels.ExploreModel
+import com.ersubhadip.helpers.Storage
 import com.ersubhadip.presenter.adapters.ExploreAdapter
 import com.ersubhadip.ww.R
 import com.ersubhadip.ww.databinding.FragmentExploreBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.net.URLEncoder
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -20,6 +26,8 @@ class ExploreFragment : Fragment() {
 
     @Inject
     lateinit var ad: ExploreAdapter
+
+    private lateinit var storage: Storage
 
     private var jList: ArrayList<ExploreModel> = ArrayList()
     private var sList: ArrayList<ExploreModel> = ArrayList()
@@ -36,11 +44,24 @@ class ExploreFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentExploreBinding.inflate(layoutInflater)
+        storage = Storage(requireContext())
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.sosBtn.setOnClickListener {
+            if (storage.lat.isNotEmpty() && storage.long.isNotEmpty()) {
+                sos()
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "Location Permission not provided",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
 
         setVerticalList()
         fetchJobList()
@@ -115,5 +136,34 @@ class ExploreFragment : Fragment() {
         sList.add(ExploreModel(1, "Scholarships 1", "Scholarships description 1", ""))
         sList.add(ExploreModel(1, "Scholarships 1", "Scholarships description 1", ""))
         sList.add(ExploreModel(1, "Scholarships 1", "Scholarships description 1", ""))
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun sos() {
+        Toast.makeText(
+            requireContext(),
+            "Message sent to ${storage.number}",
+            Toast.LENGTH_SHORT
+        ).show()
+
+        try {
+            val packageManager = requireContext().packageManager
+            val i = Intent(Intent.ACTION_VIEW)
+            val url =
+                "https://wa.me/91${storage.number}" + "?text=" + URLEncoder.encode(
+                    "https://www.google.com/maps?t=m&q=loc:${storage.lat},${storage.long}",
+                    "utf-8"
+                )
+            i.setPackage("com.whatsapp")
+            i.data = Uri.parse(url)
+            requireContext().startActivity(i)
+
+        } catch (e: Exception) {
+            Toast.makeText(
+                requireContext(),
+                "Whatsapp not found!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
